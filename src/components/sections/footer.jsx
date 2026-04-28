a20879b9257a7b087bb420b22f129f9dbdb66c79
ssau97 <ssau97@gmail.com>
2026-04-28 13:18:23 +0900

    Update footer component with new copyright and links

--- src/components/sections/footer.jsx
+++ src/components/sections/footer.jsx
@@ -8,19 +8,28 @@ const Footer = () => {
             <div className="footer-bottom pt-50 pb-40">
                 <div className="container">
                     <div className="row">
+                        {/* 좌측: 카피라이트 문구 */}
                         <div className="col-lg-6">
                             <div className="copyright-text">
                                 <p>
-                                    Copyright @{year}, <Link href="/">Bentos</Link> All
-                                    Rights Reserved.
+                                    atorz.com | 2016-{year}, @ All Rights Reserved.
                                 </p>
                             </div>
                         </div>
+                        {/* 우측: 소셜 미디어 로고 링크 */}
                         <div className="col-lg-6">
                             <div className="copyright-text extra-copyright">
-                                <p>
-                                    Crafted with ❤️ theme_ocean
-                                </p>
+                                <div className="social-links" style={{ display: 'flex', gap: '20px', justifyContent: 'flex-end' }}>
+                                    <a href="https://play.google.com/store/apps/dev?id=5233406322989418544" target="_blank" rel="noopener noreferrer" title="Android App">
+                                        <i className="fab fa-android"></i>
+                                    </a>
+                                    <a href="https://apps.apple.com/us/developer/cha-woo-lee/id1772191027" target="_blank" rel="noopener noreferrer" title="iOS App">
+                                        <i className="fab fa-apple"></i>
+                                    </a>
+                                    <a href="https://www.youtube.com/@atorz" target="_blank" rel="noopener noreferrer" title="YouTube">
+                                        <i className="fab fa-youtube"></i>
+                                    </a>
+                                </div>
                             </div>
                         </div>
                     </div>
