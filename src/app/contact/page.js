8b274db6a3111a39701dcde222bc3d8601fde9c4
Chaw <ssau97@gmail.com>
2026-05-03 21:57:48 +0900

    feat: update intro slider, icons, and contact form layout

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,28 +1,39 @@
-import React from 'react'
-import ContactOption from '../../components/sections/contact/contactOption'
-import ContactForm from '../../components/sections/contact/contactForm'
+import React from "react";
+import ContactOption from "../../components/sections/contact/contactOption";
+import ContactForm from "../../components/sections/contact/contactForm";
 
 const Contact = () => {
   return (
     <section id="contact" className="contact-area innerpage-single-area">
-      <div className="container">
-        <div className="container-inner">
-          <div className="row">
+      {/* 
+        모바일(sm 미만)에서는 padding을 0으로 만들어 양 끝에 붙입니다. 
+        px-0: 모바일에서 가로 패딩 제거
+        sm:px-3: 작은 화면 이상부터는 다시 적절한 여백 확보
+      */}
+      <div className="container px-0 sm:px-3">
+        <div
+          className="container-inner"
+          style={{ paddingLeft: "5px", paddingRight: "5px" }}
+        >
+          <div className="row mx-0">
+            {" "}
+            {/* row의 마이너스 마진으로 인한 스크롤 방지 */}
             <div className="col-xl-12 col-lg-12">
               <div className="section-title text-center wow fadeInUp delay-0-2s">
-                <p>contact</p>
-                <h2>Get in Touch with Me!</h2>
+                <p>Contact</p>
+                <h2>Turn Imagination into Reality.</h2>
               </div>
             </div>
           </div>
-          <div className="row">
+          {/* row에 mx-0를 주어 모바일에서 좌우 여백을 최소화합니다. */}
+          <div className="row mx-0">
             <ContactOption />
             <ContactForm />
           </div>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
